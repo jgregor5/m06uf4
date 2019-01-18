ae61f4349d1ec9c8aab64f40f57fb7a805ef55a5
julian <julian.gregorio@adhocware.com>
2019-01-18 11:10:58 +0100

    commander version 1.3

---
+++ src/test/InfoClientTest.java
@@ -0,0 +1,27 @@
+
+package test;
+
+import client.CommanderClient;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author julian
+ */
+public class InfoClientTest {
+    
+    private static final Logger LOGGER = Logger.getLogger(InfoClientTest.class.getName());
+    
+    public static void main(String[] args) {
+        
+        CommanderClient commander = new CommanderClient("localhost");
+        commander.connect(9000);
+        
+        JSONObject response = commander.execute(new JSONObject().put("command", "info.components"));  
+        LOGGER.log(Level.INFO, "response:{0}", response.toString(4));
+        
+        commander.disconnect();
+    }
+}
